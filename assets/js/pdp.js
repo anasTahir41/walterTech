14743217cb16794bd27ddf230b84244507379473
nabeeliftikhar0 <tehreem.bhatti@nestosh.com>
2023-03-31 19:41:31 +0500

    product detail page
    Product detail page - Done

---
+++ assets/js/pdp.js
@@ -0,0 +1,54 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.product-carousel__wrapper').slick({
+        dots: false,
+        infinite: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+        ]
+    });
+
+    $('.slider-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.slider-thumb'
+      });
+      $('.slider-thumb').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-content',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true
+      });
+});
