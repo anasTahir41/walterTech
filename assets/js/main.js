57c69165a5c2625d80b29e0495ee93d93a8d1189
nabeeliftikhar0 <tehreem.bhatti@nestosh.com>
2023-06-14 12:19:49 +0500

    Cart page, Where to buy page Done
    - Added cart page's HTML and style the elements
    - Added where to buy page's HTML and style the elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,4 +151,30 @@ $(document).ready(function () {
         e.stopPropagation();
         e.preventDefault();
     });
+
+    $(document).ready(function() {
+        $('.num-in span').click(function () {
+            var $input = $(this).parents('.num-block').find('input.in-num');
+          if($(this).hasClass('minus')) {
+            var count = parseFloat($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            if (count < 2) {
+              $(this).addClass('dis');
+            }
+            else {
+              $(this).removeClass('dis');
+            }
+            $input.val(count);
+          }
+          else {
+            var count = parseFloat($input.val()) + 1
+            $input.val(count);
+            if (count > 1) {
+              $(this).parents('.num-block').find(('.minus')).removeClass('dis');
+            }
+          }
+          $input.change();
+          return false;
+        });
+    });
 });
