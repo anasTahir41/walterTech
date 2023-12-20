598408fd41e92b11fde58644fade6a11bcb5d8be
Anas Tahir <anas.tahir@nestosh.com>
2023-12-20 17:32:35 +0500

    Added history, automation and smoothscroll changes

--- assets/js/pdp.js
+++ assets/js/pdp.js
@@ -48,7 +48,15 @@ $(document).ready(function () {
         slidesToScroll: 1,
         asNavFor: '.slider-content',
         dots: false,
+        arrows: false,
         centerMode: false,
         focusOnSelect: true
     });
+
+    // Get the slide count of the content slider
+    var slideCount = $('.slider-content').slick('getSlick').slideCount;
+    console.log('Count' + slideCount);
+
+    // Set the updated slidesToShow value for the thumbnail slider
+    $('.slider-thumb').slick('slickSetOption', 'slidesToShow', slideCount - 1, true);
 });
